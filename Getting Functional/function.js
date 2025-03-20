7316d3584c5e79ed6d0b05cdb00a309451f5cd3e
mcjersyd <mcdelacruz@filamer.edu.ph>
2025-03-20 02:16:49 -0700

    New Version

---
+++ Getting Functional/function.js
@@ -0,0 +1,13 @@
+var dogName = "rover";
+var dogWeight = 19;
+
+function bark( name, weight){
+    if (Weight > 20) {
+        console.log(name + "says WOOF WOOF");
+    } else{
+        console.log(name + "says woof woof");
+    }
+}
+
+bark ("amllows", 50)
+bark ("skkrskkr", 12)
